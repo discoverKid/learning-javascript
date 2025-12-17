a007c464696800a85b5bb4317937ff1786702c92
Tony <tony.haugabrook21@gmail.com>
2025-12-17 16:29:47 -0500

    Adding some file changes

--- conditionals.js
+++ conditionals.js
@@ -1,3 +1,6 @@
+const prompt = require("prompt-sync")()
+
+
 let x = 4
 let y = 5
 
@@ -9,4 +12,16 @@ if(x > y) {
 console.log("x is greater than y!")
 } else {
 console.log("Nope, x is not greater than y.")
+}
+
+// a program to determine whether it's time to get up
+
+let hour = prompt("What time is it? ")
+
+if(hour < 4) {
+    console.log("It's the middle of the night-keep sleeping!")
+} else if(hour > 4 && hour < 7) {
+        console.log("You can sleep a little longer.")
+    } else {
+        console.log("Why are you still sleeping? Get up!")
 }
