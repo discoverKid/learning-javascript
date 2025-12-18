0174e0666ab66ddfb1a8fe58e134a6868c1d329c
Tony <tony.haugabrook21@gmail.com>
2025-12-18 15:00:31 -0500

    Added loops.js file

---
+++ loops.js
@@ -0,0 +1,15 @@
+// This is a for loop.
+
+// first part - creates a counter variable.
+// second part - sets the condition in which the loop will execute.
+// third part - increments the counter variable.
+
+for(x = 1; x < 5 ; x++){
+console.log("x is " + x)
+}
+
+let y = "foo"
+for(x = 1; x < 5 ; x++) {
+    y = y + "bar"
+    console.log(y)
+}
