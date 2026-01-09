43a74e4623a8b35ab3933d3c7a2ed6ca40b90c70
Tony <tony.haugabrook21@gmail.com>
2026-01-09 15:02:49 -0500

    Added classes.js file

---
+++ classes.js
@@ -0,0 +1,15 @@
+class cat{
+    constructor(name, age) {
+        this.name = name
+        this.age = age
+    }
+}
+
+const myfirstCat = new cat('Fluffy', 5)
+console.log(myfirstCat)
+console.log(myfirstCat.name)
+
+console.log(myfirstCat.age)
+
+const mySecondCat = new cat('Ginger', 9)
+console.log(mySecondCat.age)
