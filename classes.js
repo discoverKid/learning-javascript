4955ddd32b91add976b8c4812fdde7c8499e22de
Tony <tony.haugabrook21@gmail.com>
2026-01-13 15:24:41 -0500

    Modified classes.js file

--- classes.js
+++ classes.js
@@ -3,6 +3,14 @@ class cat{
         this.name = name
         this.age = age
     }
+    meow() {
+        console.log('Meow!') //log out what the cat does which is the 'method'
+    }
+    scratch(numberOfTimes) {
+        for(let x = 1; x <= numberOfTimes; x++) {
+            console.log('Scratch')
+        } // increment the x value above every time the loop runs.
+    }
 }
 
 const myfirstCat = new cat('Fluffy', 5)
@@ -13,3 +21,8 @@ console.log(myfirstCat.age)
 
 const mySecondCat = new cat('Ginger', 9)
 console.log(mySecondCat.age)
+
+myfirstCat.meow()
+mySecondCat.meow()
+
+myfirstCat.scratch(4)
